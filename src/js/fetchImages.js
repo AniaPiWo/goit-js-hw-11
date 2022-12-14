4c056a0e91cf66a99efc0313833a63bd6e3cee98
AniaPiWo <snowflake@wp.pl>
2022-12-14 13:29:24 +0100

    axios fix

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,4 +1,17 @@
+import axios from "axios";
+
 export const fetchImages = async (inputData, page) => {
+  try {
+    const response = await axios.get(
+    `https://pixabay.com/api/?key=32017695-e686be41b07e014b2e19f4e7c&q=${inputData}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`
+  );
+  return response.data;
+} catch (error) {
+  console.error(error);
+}}
+
+// alternative ver
+/* export const fetchImages = async (inputData, page) => {
     return await fetch(
       `https://pixabay.com/api/?key=32017695-e686be41b07e014b2e19f4e7c&q=${inputData}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`
     )
@@ -14,19 +27,4 @@ export const fetchImages = async (inputData, page) => {
       .catch(error => {
         console.error(error);
       });
-  };
-
-  
-/* import axios from "axios";
-
-
-export const fetchImages = async (inputData, page) => {
-  try {
-    const response = await axios.get(
-    `https://pixabay.com/api/?key=32017695-e686be41b07e014b2e19f4e7c&q=${inputData}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`
-  );
-  return response;
-} catch (error) {
-  console.error(error);
-}}
- */
+  }; */
